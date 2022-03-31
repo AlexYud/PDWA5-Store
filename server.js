97c662d65dc93102129ee8863e974e031ba2d98d
Alexandre Yudi <alexyud@hotmail.com>
2022-03-30 21:58:08 -0300

    deploy heroku update

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/pdwa5-store'));
-app.get('/*', function (req, res) {
-  res.sendFile(path.join('/src/index.html'));
+app.use(express.static('www'));
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
 });
-app.listen(process.env.PORT || 8080);
+
+app.set('port', process.env.PORT || 5000);
+
+app.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
