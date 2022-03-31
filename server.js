49ff2b33d4ba98ebb106ac679563f98ad4af63ae
Alexandre Yudi <alexyud@hotmail.com>
2022-03-30 21:44:13 -0300

    deploy heroku 3

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist/pdwa5-store'));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname +
-    '/dist/pdwa5-store/index.html'));
+    '/dist/pdwa5-store/src/index.html'));
 });
 app.listen(process.env.PORT || 8080);
