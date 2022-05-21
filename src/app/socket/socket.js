dcd18c8f966c5a9f84c45e25f568e5296db9ca5f
Alexandre Yudi <alexyud@hotmail.com>
2022-05-21 18:10:47 -0300

    Finalizando projeto

--- src/app/socket/socket.js
+++ src/app/socket/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client'
 
-var url = "ws://localhost:3000/"
+var url = "ws://pdwstoreapi.herokuapp.com/"
 
 export default io(url, { transports: ['websocket'], reconnection: true})
